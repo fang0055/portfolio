1a5cfa18ef09d3612cd67cf2d35ec216ab319f49
Zhou Link Fang <linkfang@outlook.com>
2020-09-19 14:48:14 -0400

    Updates

--- precache-manifest.7757dd573af57fd0598705d087527c09.js
+++ precache-manifest.7757dd573af57fd0598705d087527c09.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb6ef6c072600524750e5481d7ae5936",
+    "revision": "4e83064f05afe65e332cc3d96c23e8b3",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "800d094c7c704c8c1ae3",
-    "url": "/portfolio/static/css/main.79828d19.chunk.css"
+    "revision": "218c5788dbe61cb99a41",
+    "url": "/portfolio/static/css/main.f9c76e50.chunk.css"
   },
   {
     "revision": "c16fa69ce844d1b27afb",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.8f2a3a58.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "800d094c7c704c8c1ae3",
-    "url": "/portfolio/static/js/main.fda63263.chunk.js"
+    "revision": "218c5788dbe61cb99a41",
+    "url": "/portfolio/static/js/main.ce9cfe8a.chunk.js"
   },
   {
     "revision": "6c1d3ad26a766dc494c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f7f7b9de189924fbd02d98261c12bad2.js"
+  "/portfolio/precache-manifest.7757dd573af57fd0598705d087527c09.js"
 );
 
 self.addEventListener('message', (event) => {
